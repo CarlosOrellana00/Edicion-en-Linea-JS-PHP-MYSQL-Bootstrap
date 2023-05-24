95436eeaf4e0e147b07610bcf02cb1a6144f7c8b
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-23 20:25:33 -0400

    6

---
+++ assets/php/process.php
@@ -0,0 +1,12 @@
+<?php
+  include 'connection.php'
+
+  if(isset($_POST["pk"])){
+    $query = "UPDATE tbl_sample SET ".$_POST['name']." = "
+    .$_POST["value"]."'WHERE id ='".$_POST["pk"]."'";
+
+    $connect->query($query);
+
+    echo 'done';
+  }
+?>
